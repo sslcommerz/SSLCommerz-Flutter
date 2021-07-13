41c4dcc45be35136747c30578dd933a0bcf88045
Md. Rakibul Islam <rakibul.islam@sslwireless.com>
2021-07-13 14:01:32 +0600

    update example code for v2.0.1

---
+++ example/android/app/src/main/kotlin/com/example/test_sslcommerz_sdk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_sslcommerz_sdk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:sslcommerz_flutter/model/SSLCAdditionalInitializer.dart';
-import 'package:sslcommerz_flutter/model/SSLCCustomerInfoInitializer.dart';
-import 'package:sslcommerz_flutter/model/SSLCEMITransactionInitializer.dart';
-import 'package:sslcommerz_flutter/model/SSLCSdkType.dart';
-import 'package:sslcommerz_flutter/model/SSLCShipmentInfoInitializer.dart';
-import 'package:sslcommerz_flutter/model/SSLCTransactionInfoModel.dart';
-import 'package:sslcommerz_flutter/model/SSLCommerzInitialization.dart';
-import 'package:sslcommerz_flutter/model/SSLCurrencyType.dart';
-import 'package:sslcommerz_flutter/model/sslproductinitilizer/General.dart';
-import 'package:sslcommerz_flutter/model/sslproductinitilizer/SSLCProductInitializer.dart';
-import 'package:sslcommerz_flutter/sslcommerz.dart';
+import 'package:flutter_sslcommerz/model/SSLCAdditionalInitializer.dart';
+import 'package:flutter_sslcommerz/model/SSLCCustomerInfoInitializer.dart';
+import 'package:flutter_sslcommerz/model/SSLCEMITransactionInitializer.dart';
+import 'package:flutter_sslcommerz/model/SSLCSdkType.dart';
+import 'package:flutter_sslcommerz/model/SSLCShipmentInfoInitializer.dart';
+import 'package:flutter_sslcommerz/model/SSLCTransactionInfoModel.dart';
+import 'package:flutter_sslcommerz/model/SSLCommerzInitialization.dart';
+import 'package:flutter_sslcommerz/model/SSLCurrencyType.dart';
+import 'package:flutter_sslcommerz/model/sslproductinitilizer/General.dart';
+import 'package:flutter_sslcommerz/model/sslproductinitilizer/SSLCProductInitializer.dart';
+import 'package:flutter_sslcommerz/sslcommerz.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,141 +27,138 @@ class _MyAppState extends State<MyApp> {
   dynamic formData = {};
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('SSLCommerz'),
-          ),
-          body: Form(
-            key: _key,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        initialValue: "testbox",
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8.0))),
-                          hintText: "Store ID",
-                        ),
-                        validator: (value) {
-                          if (value.isEmpty)
-                            return "Please input store id";
-                          else
-                            return null;
-                        },
-                        onSaved: (value) {
-                          formData['store_id'] = value;
-                        },
+        appBar: AppBar(
+          title: const Text('SSLCommerz'),
+        ),
+        body: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      initialValue: "testbox",
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(8.0))),
+                        hintText: "Store ID",
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        initialValue: "qwerty",
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8.0))),
-                          hintText: "Store password",
-                        ),
-                        validator: (value) {
-                          if (value.isEmpty)
-                            return "Please input store password";
-                          else
-                            return null;
-                        },
-                        onSaved: (value) {
-                          formData['store_password'] = value;
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        keyboardType: TextInputType.phone,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8.0))),
-                          hintText: "Phone number",
-                        ),
-                        onSaved: (value) {
-                          formData['phone'] = value;
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        initialValue: "10",
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8.0))),
-                          hintText: "Payment amount",
-                        ),
-                        validator: (value) {
-                          if (value.isEmpty)
-                            return "Please input amount";
-                          else
-                            return null;
-                        },
-                        onSaved: (value) {
-                          formData['amount'] = double.parse(value);
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8.0))),
-                          hintText: "Enter multi card",
-                        ),
-                        onSaved: (value) {
-                          formData['multicard'] = value;
-                        },
-                      ),
-                    ),
-                    RaisedButton(
-                      child: Text("Pay now"),
-                      onPressed: () {
-                        if (_key.currentState.validate()) {
-                          _key.currentState.save();
-                          // sslCommerzGeneralCall();
-                          sslCommerzCustomizedCall();
-                        }
+                      validator: (value) {
+                        if (value != null)
+                          return "Please input store id";
+                        else
+                          return null;
                       },
-                    )
-                  ],
-                ),
+                      onSaved: (value) {
+                        formData['store_id'] = value;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      initialValue: "qwerty",
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(8.0))),
+                        hintText: "Store password",
+                      ),
+                      validator: (value) {
+                        if (value != null)
+                          return "Please input store password";
+                        else
+                          return null;
+                      },
+                      onSaved: (value) {
+                        formData['store_password'] = value;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(8.0))),
+                        hintText: "Phone number",
+                      ),
+                      onSaved: (value) {
+                        formData['phone'] = value;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      initialValue: "10",
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(8.0))),
+                        hintText: "Payment amount",
+                      ),
+                      validator: (value) {
+                        if (value != null)
+                          return "Please input amount";
+                        else
+                          return null;
+                      },
+                      onSaved: (value) {
+                        formData['amount'] = double.parse(value!);
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(8.0))),
+                        hintText: "Enter multi card",
+                      ),
+                      onSaved: (value) {
+                        formData['multicard'] = value;
+                      },
+                    ),
+                  ),
+                  ElevatedButton(
+                    child: Text("Pay now"),
+                    onPressed: () {
+                      if (_key.currentState != null) {
+                        _key.currentState?.save();
+                        // sslCommerzGeneralCall();
+                        sslCommerzCustomizedCall();
+                      }
+                    },
+                  )
+                ],
               ),
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 
   Future<void> sslCommerzGeneralCall() async {
     Sslcommerz sslcommerz = Sslcommerz(
         initializer: SSLCommerzInitialization(
+            //Use the ipn if you have valid one, or it will fail the transaction.
             ipn_url: "www.ipnurl.com",
             multi_card_name: formData['multicard'],
             currency: SSLCurrencyType.BDT,
@@ -176,6 +174,7 @@ class _MyAppState extends State<MyApp> {
   Future<void> sslCommerzCustomizedCall() async {
     Sslcommerz sslcommerz = Sslcommerz(
         initializer: SSLCommerzInitialization(
+            //Use the ipn if you have valid one, or it will fail the transaction.
             ipn_url: "www.ipnurl.com",
             multi_card_name: formData['multicard'],
             currency: SSLCurrencyType.BDT,
@@ -201,12 +200,13 @@ class _MyAppState extends State<MyApp> {
                     shipPostCode: "7860")))
         .addCustomerInfoInitializer(
             customerInfoInitializer: SSLCCustomerInfoInitializer(
-                customerName: null,
-                customerEmail: null,
-                customerAddress1: null,
-                customerCity: null,
-                customerPostCode: null,
-                customerCountry: null,
+                customerState: "Chattogram",
+                customerName: "Abu Sayed Chowdhury",
+                customerEmail: "sayem227@gmail.com",
+                customerAddress1: "Anderkilla",
+                customerCity: "Chattogram",
+                customerPostCode: "200",
+                customerCountry: "Bangladesh",
                 customerPhone: formData['phone']))
         .addProductInitializer(
             sslcProductInitializer:
@@ -281,9 +281,20 @@ class _MyAppState extends State<MyApp> {
                 valueD: "value d"));
     var result = await sslcommerz.payNow();
     if (result is PlatformException) {
-      print("the response is: " + result.message + " code: " + result.code);
+      print("the response is: " +
+          result.message.toString() +
+          " code: " +
+          result.code);
     } else {
       SSLCTransactionInfoModel model = result;
+      Fluttertoast.showToast(
+          msg: "Transaction successful: Amount ${model.amount} TK",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.black,
+          textColor: Colors.white,
+          fontSize: 16.0);
     }
   }
 }
